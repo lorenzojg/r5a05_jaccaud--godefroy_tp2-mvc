2e9cecdf6890d8ea6288bdc12fb37418a58c5006
bl <bouchkhaf@gmail.com>
2025-11-10 10:21:57 +0100

    First commit

---
+++ src/main/java/fr/devavance/metier/views/interfaces/IServletView.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package fr.devavance.metier.views.interfaces;
+
+/**
+ *
+ * @author bouchaib.lemaire
+ */
+public interface IServletView {
+    String CONTROLLER = "controller";
+    String FORM_LOGOUT = "/WEB-INF/form_logout.html"; 
+}
